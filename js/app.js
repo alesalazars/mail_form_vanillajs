04fd0b5d1038d9344067e33303359977710eb32d
Ale <ma.salazarsanchez@gmail.com>
2020-05-20 00:07:02 -0400

    Submit and reset button working

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const email = document.getElementById('email');
 const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
+const formularioEnviar = document.getElementById('enviar-mail');
+const resetBtn = document.getElementById('resetBtn');
 
 
 
@@ -17,6 +19,12 @@ function eventListeners(){
   email.addEventListener('blur', validarCampo);
   asunto.addEventListener('blur', validarCampo);
   mensaje.addEventListener('blur', validarCampo);
+
+  // Boton de enviar en el submit
+  formularioEnviar.addEventListener('submit', enviarEmail);
+
+  // Boton de reset
+  resetBtn.addEventListener('click', resetFormulario);
 }
 
 
@@ -27,6 +35,7 @@ function inicioApp(){
   // Desahibilitar el envio
   btnEnviar.disabled = true;
 }
+
 
 // Valida que le campo tenga algo escrito
 function validarCampo(){
@@ -46,9 +55,43 @@ function validarCampo(){
   }
 }
 
-function validarLongitud(campo){
-  console.log(campo.value.length);
 
+// Resetear el formulario
+function resetFormulario(e){
+  formularioEnviar.reset();
+  e.preventDefault();
+}
+
+
+// Cuando se envia el correo
+function enviarEmail(e){
+  // Spinner al presionar enviar
+  const spinnerGif = document.querySelector('#spinner');
+  spinnerGif.style.display = 'block';
+
+  // Gif que envia email
+  const enviado = document.createElement('img');
+  enviado.src = 'img/mail.gif';
+  enviado.style.display = 'block';
+
+  // Ocultar spinner y mostrar gif de enviado
+  setTimeout(function(){
+    spinnerGif.style.display = 'none';
+    document.querySelector('#loaders').appendChild(enviado);
+
+    setTimeout(function(){
+      enviado.remove();
+      formularioEnviar.reset();
+    }, 5000);
+
+  }, 3000);
+
+  e.preventDefault();
+}
+
+
+// Verifica la longitud del texto en los campos
+function validarLongitud(campo){
   if(campo.value.length > 0){
     campo.style.borderBottomColor = 'green';
     campo.classList.remove('error');
